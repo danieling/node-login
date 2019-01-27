2f4a3fe8d52a6d757acfca2190f722872b24e385
Daniel Rocha <to.danielrp@gmail.com>
2019-01-27 08:30:42 -0400

    basic server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(morgan('dev'));
+
+
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port 4000')
+})
